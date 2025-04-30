b9106b60b02301f2157ab28c87c1adcb2bdb4bcb
Gabriele Logiurato <gabriele.logiurato@lernende.bbw.ch>
2025-04-30 13:39:15 +0200

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Es hat funktioniert
+          Version 1.0
         </p>
         <a
           className="App-link"
